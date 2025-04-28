85db2b6a74dcfd98cbd4a3b57176c1403f4d840d
sboun <stbounyavong@ucsd.edu>
2025-04-28 16:26:18 -0400

    Start frontend

---
+++ frontend/src/router.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx"
+import HomePage from "./pages/HomePage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
+
+export default router;
