cc53aa63652b280d2f287470c32a7cef43538054
kenny <knywelcome@gmail.com>
2025-04-29 09:45:10 -0500

    config of tailwind and bootstrap

--- frontend/src/pages/ChallengesHomePage.jsx
+++ frontend/src/pages/ChallengesHomePage.jsx
@@ -5,16 +5,7 @@ function ChallengesHomePage() {
     <h1>this is Challenges home page</h1>
     
     
-    <h1></h1>
-    <div>
-    <h1>name</h1>
     
-    <div>
-        <h1>pass%</h1>
-        <h1>level</h1>
-        <h1>result</h1>
-    </div>
-    </div>
     </>
 )
 };
