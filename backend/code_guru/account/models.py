9436ed21789cf952b1f689d1ef65d5c686f9d8cc
bitterguy-code <jasonb4cake@gmail.com>
2025-04-29 14:33:38 -0400

    Changed secret key and added it to a .env. Laid out first basic models.

--- backend/code_guru/account/models.py
+++ backend/code_guru/account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    position = models.TextField()
+    years = models.IntegerField()
+    goals = models.TextField()
+    
