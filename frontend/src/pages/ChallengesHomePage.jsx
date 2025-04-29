05b780f7215eb1b737b45384e854c32b1482acb7
kenny <knywelcome@gmail.com>
2025-04-29 09:30:33 -0500

    test

--- frontend/src/pages/ChallengesHomePage.jsx
+++ frontend/src/pages/ChallengesHomePage.jsx
@@ -1,3 +1,22 @@
-export default function ChallengesHomePage() {
-    return <h1>this is Challenges home page</h1>;
-  }
+function ChallengesHomePage() {
+
+    return ( 
+    <>
+    <h1>this is Challenges home page</h1>
+    
+    
+    <h1></h1>
+    <div>
+    <h1>name</h1>
+    
+    <div>
+        <h1>pass%</h1>
+        <h1>level</h1>
+        <h1>result</h1>
+    </div>
+    </div>
+    </>
+)
+};
+
+  export default  ChallengesHomePage;
