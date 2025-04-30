f4c5988e41e1eb190e90f001f65942badf073e8d
kenny <knywelcome@gmail.com>
2025-04-30 08:59:58 -0500

    quick save

--- frontend/src/pages/ChallengesHomePage.jsx
+++ frontend/src/pages/ChallengesHomePage.jsx
@@ -3,7 +3,93 @@ export default function ChallengesHomePage() {
     return ( 
     <>
     <h1>this is Challenges home page</h1>
+    {/* <div className="w-screen h-screen bg-gray-100"> */}
+      {/* <iframe className="w-5/6 h-5/6" src="https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/"></iframe> */}
+
+
+  <div className="flex">
+    <div className=" mx-auto p-4 sm:p-6 lg:p-8 bg-white rounded-xl shadow-md">
+      <p className="text-base sm:text-lg mb-4 break-words">
+        Given an array 
+        <code className="bg-gray-100 px-1 py-0.5 rounded ">nums</code> of integers,
+        return how many of them contain an
+        <strong className=" font-semibold"> even number </strong>
+        of digits.
+      </p>
+
+      <div className="mb-6">
+      <h2 className="text-lg font-bold text-indigo-700 mb-2">
+        <strong className="block">Example 1:</strong>
+      </h2>
+      <pre className="bg-gray-50 border border-gray-200 rounded p-4 overflow-x-auto text-sm break-words">
+        <strong className="block">Input:</strong>
+          nums = [12, 345, 2, 6, 7896]
+
+        <strong className="block">Output:</strong>
+          2
+
+        <strong className="block">Explanation:</strong>
+        <p>12 contains 2 digits (even number of digits).</p>
+        <p>345 contains 3 digits (odd number of digits).</p>
+        <p>2 contains 1 digit (odd number of digits).</p>
+        <p>6 contains 1 digit (odd number of digits).</p>
+        <p>7896 contains 4 digits (even number of digits).</p>
+        <p>Therefore only 12 and 7896 contain an even number of digits.</p>
+      </pre>
+      </div>
+
+      <div className="mb-6">
+      <h2 className="text-lg font-bold text-indigo-700 mb-2">
+        <strong className="block">Example 2:</strong>
+      </h2>
+      <pre className="bg-gray-50 border border-gray-200 rounded p-4 overflow-x-auto text-sm break-words">
+        <strong className="block">Input:</strong>
+        nums = [555, 901, 482, 1771]
+
+        <strong className="block">Output:</strong>
+          1
+
+          <strong className="block">Explanation:</strong>
+          <strong className="block">Only 1771 contains an even number of digits.</strong>
+      </pre>
+      </div>
+
+      <div>
+        <h2 className="text-lg font-bold text-indigo-700 mb-2">
+          <strong className="block">Constraints:</strong>
+        </h2>
+        <ul className="list-disc pl-5 space-y-1 text-sm sm:text-base break-words">
+          <li>
+            <code className="bg-gray-100 px-1 py-0.5 rounded block">
+              1 &lt;= nums.length &lt;= 500
+            </code>
+          </li>
+          <li>
+            <code className="bg-gray-100 px-1 py-0.5 rounded block">
+              1 &lt;= nums[i] &lt;= 10<sup>5</sup>
+            </code>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+
+    {/* second columz */}
+    <div className="w-1/2">
+      <div className="max-w-3xl mx-auto p-4 sm:p-6 lg:p-8 bg-white rounded-xl shadow-md">
+
+      </div>
+      <div className="max-w-3xl mx-auto p-4 sm:p-6 lg:p-8 bg-white rounded-xl shadow-md">
+        
+      </div>
+    </div>
     
+  </div>
+  
+
+
+
+    {/* </div> */}
     
     
     </>
