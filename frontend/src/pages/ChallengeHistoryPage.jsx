95416fcb17620c3887d767e09fb23875bcaf8002
sboun <stbounyavong@ucsd.edu>
2025-05-06 16:27:03 -0400

    Challenge history page rough draft

--- frontend/src/pages/ChallengeHistoryPage.jsx
+++ frontend/src/pages/ChallengeHistoryPage.jsx
@@ -1,6 +1,7 @@
 import "./challengehistory.css";
-import ChallengeCard from "../components/ChallengeCard";
+import { useState, useEffect } from "react";
 
+import grayCircle from "../challengeAssets/grayCircle.png";
 import ninjaStar from "../challengeAssets/ninjaStar.png";
 import trainingDummy from "../challengeAssets/trainingDummy.png";
 import katana from "../challengeAssets/katana.png";
@@ -13,19 +14,40 @@ import gong from "../challengeAssets/gong.png";
 import suit from "../challengeAssets/suit.png";
 
 export default function ChallengeHistoryPage() {
+  const [challenges, setChallenges] = useState([]);
+  const weaponIcons = [
+    suit,
+    trainingDummy,
+    katana,
+    sai,
+    boStaff,
+    nunchaku,
+    kusarigama,
+    bokken,
+    gong,
+    ninjaStar,
+  ];
+
+  //useEffect on load - get 10 recent finished challenges, put into challenges
+
+  //create 10 positions for each challenge
+  const positions = weaponIcons.map((icon, i) => ({
+    icon: challenges[i] ? icon : grayCircle,
+    challenge: challenges[i] || null,
+  }));
+
   return (
     <div className="history_container">
       <div className="history_weapons">
-        <img src={ninjaStar} id="weapon1"></img>
-        <img src={trainingDummy} id="weapon2"></img>
-        <img src={katana} id="weapon3"></img>
-        <img src={sai} id="weapon4"></img>
-        <img src={boStaff} id="weapon5"></img>
-        <img src={nunchaku} id="weapon6"></img>
-        <img src={kusarigama} id="weapon7"></img>
-        <img src={bokken} id="weapon8"></img>
-        <img src={gong} id="weapon9"></img>
-        <img src={suit} id="weapon10"></img>
+        {positions.map(({ icon, challenge }, i) => (
+          <img
+            src={icon}
+            id={`weapon${i + 1}`}
+            alt={challenge ? "Weapon" : "Hidden weapon"}
+            // add href to link to challenges page
+            // add hover to see title of weapon/challenge or "weapon not unlocked" for absence of challenge
+          />
+        ))}
       </div>
     </div>
   );
